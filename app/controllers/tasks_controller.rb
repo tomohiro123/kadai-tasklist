97c63d641558a992a9a558384d0ffa188a6514e5
ogi123 <tmhrogwr123@yahoo.co.jp>
2019-05-13 06:09:18 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,10 @@
 class TasksController < ApplicationController
+  before_action :correct_user, only: [:destroy]
   def index
-    @tasks=Task.all
+    if logged_in?
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order(id: :desc)
+    end
   end
 
   def show
@@ -12,7 +16,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+     @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
@@ -29,6 +33,7 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
+    @task = current_user.tasks.build(task_params)
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -41,13 +46,22 @@ class TasksController < ApplicationController
 
   def destroy
     @task = Task.find(params[:id])
-    @task.destroy
-
+    @task.destroy 
+    
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
+ 
   private
   def task_params
-    params.require(:task).permit(:content,:status)
+    params.require(:task).permit(:content,:status,:user)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      flash[:danger]='権限がありません'
+      redirect_to root_url
+    end 
   end
 end
